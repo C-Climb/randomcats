c166217e335967bf308f2e86f8b9b6b9d4251a47
Christian Stephens <christianthomasstephens@gmail.com>
2019-08-07 18:00:52 -0700

    Added cat facts & more api reqs.

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,31 @@
-const express = require('express');
-const router = express.Router();
-const axios = require('axios');
+const express    = require('express');
+const router     = express.Router();
+const axios      = require('axios');
 const { catAPI } = require('./../config.json');
 
 // Main route
 router.get('/', (req, res) => {
-    const token = catAPI;
-    axios.get("https://api.thecatapi.com/v1/images/search?limit=1&page=0&order=Desc", ({
-            headers: [{
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                Authorization: 'Bearer ' + token
-            }]
-        }))
-        .then((response) => {
-            res.render('landing',{catImages: response.data});
-        })
-        .catch((error) => {
-            console.log(error);
-        });
+    // const token = catAPI;
+    //         headers: [{
+    //             'Accept': 'application/json',
+    //             'Content-Type': 'application/json',
+    //             Authorization: 'Bearer ' + token
+    //         }]
+    axios.all([
+        axios.get("https://api.thecatapi.com/v1/images/search"),
+        axios.get("https://api.thecatapi.com/v1/images/search?limit=5&page=10&order=Desc"), //Will need to manually add headers to every request if i want to use the apis voting funcctionality!
+        axios.get("https://catfact.ninja/fact")
+    ])
+    .then(axios.spread((randCatImages, descCatImages, catFacts) =>{
+        console.log(catFacts.data.fact);
+        res.render('landing',{randCatImages: randCatImages.data, descCatImages:descCatImages.data, catFacts:catFacts.data.fact});
+    })); 
 });
+// TODO
+// refresh page on next click so our 'previous' button can manipulate window history
 
 router.get('/about', (req, res) => {
+    // TODO - display a random cat (or dog((maybe))) image as the background image, add an about me and link to a reputable animal and suicide charity.
     res.render('about');
 });
 
