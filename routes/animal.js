ff1d364c00b7702ebcff925abda7a0eb740bd57f
Christian Stephens <christianthomasstephens@gmail.com>
2019-08-29 14:32:26 -0700

    Changed dog API. Refactored CSS again.

--- routes/animal.js
+++ routes/animal.js
@@ -23,8 +23,8 @@ router.get('/dog', (req, res) =>{
     }
 
     axios.all([
-        axios.get("https://api.thedogapi.com/v1/images/search").catch(errorHandler),
-        axios.get("https://api.thedogapi.com/v1/images/search?limit=1&page=10&order=Desc").catch(errorHandler)
+        axios.get("https://dog.ceo/api/breeds/image/random").catch(errorHandler),
+        axios.get("https://dog.ceo/api/breeds/image/random").catch(errorHandler)
     ])
     .then(axios.spread((randDogImages,nextDogImage) =>{
         res.render('dog', {randDogImages:randDogImages.data, nextDogImage:nextDogImage.data});
