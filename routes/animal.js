c67f65e86feafa365466340b9d8b45f7166840e9
Christian Stephens <christianthomasstephens@gmail.com>
2019-08-31 13:42:12 -0700

    Added dog facts api again. Fixed css bugs.

--- routes/animal.js
+++ routes/animal.js
@@ -10,7 +10,7 @@ router.get('/cat', (req, res) => {
     axios.all([
         axios.get("https://api.thecatapi.com/v1/images/search").catch(errorHandler),
         axios.get("https://api.thecatapi.com/v1/images/search?limit=1&page=10&order=Desc").catch(errorHandler),
-        axios.get("https://catfact.ninja/fact").catch(errorHandler)
+        axios.get("https://catfact.ninja/fact?max_length=100").catch(errorHandler)
     ])
     .then(axios.spread((randCatImages, nextCatImages, catFacts) =>{
         res.render('cat',{randCatImages: randCatImages.data, nextCatImages:nextCatImages.data, catFacts:catFacts.data.fact});
@@ -24,10 +24,11 @@ router.get('/dog', (req, res) =>{
 
     axios.all([
         axios.get("https://api.thedogapi.com/v1/images/search").catch(errorHandler),
-        axios.get("https://api.thedogapi.com/v1/images/search?limit=1&order=Desc").catch(errorHandler)
+        axios.get("https://api.thedogapi.com/v1/images/search?limit=1&order=Desc").catch(errorHandler),
+        axios.get("http://dog-api.kinduff.com/api/facts").catch(errorHandler)
     ])
-    .then(axios.spread((randDogImages,nextDogImage) =>{
-        res.render('dog', {randDogImages:randDogImages.data, nextDogImage:nextDogImage.data});
+    .then(axios.spread((randDogImages,nextDogImage,dogFacts) =>{
+        res.render('dog', {randDogImages:randDogImages.data, nextDogImage:nextDogImage.data, dogFacts:dogFacts.data});
     }));
 });
 
